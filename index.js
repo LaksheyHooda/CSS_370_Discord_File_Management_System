ea6dbe36771ee7df434f5e5f1ab817298d88a0b5
LaksheyHooda <lakshey2016@gmail.com>
2024-05-24 21:02:54 -0700

    Added slash command support

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { Client, GatewayIntentBits, Events } from 'discord.js';
+import { Client, GatewayIntentBits, Events, SlashCommandBuilder, Collection } from 'discord.js';
 import fetch from 'node-fetch';
 import fs from 'fs';
-import { fileURLToPath } from 'url';
 import path from 'path';
 
 
@@ -17,6 +16,48 @@ const client = new Client({
         GatewayIntentBits.MessageContent,
     ],
     partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+});
+
+client.commands = new Collection();
+
+const foldersPath = path.join('commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = await import(`./${filePath}`);
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
 });
 
 client.on(Events.MessageCreate, async (message) => {
@@ -43,13 +84,6 @@ client.on(Events.MessageCreate, async (message) => {
                 message.channel.send(`Attachment received: ${attachment.url}`);
             });
         }
-
-        // const channel = message.channel; // This channel is where the message was sent
-        // if (channel) {
-        //     await channel.send(`${message.content} Your mom`).catch(console.error);
-        // } else {
-        //     console.log('Unexpectedly, the channel could not be found');
-        // }
     }
 });
 
